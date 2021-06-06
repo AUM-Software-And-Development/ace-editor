e3865b588f3dc27520c4b4c29ceb7d991f4bf32a
abm <atolbirdmaker@gmail.com>
2021-06-06 10:24:08 -0600

    server validation

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,1 +1,29 @@
-ÿþ
+import * as _express_ from "express";
+import * as _bodyparser_ from "body-parser";
+
+const PORT = 3000;
+
+class ExpressApp
+{
+
+    public app: _express_.Application;
+
+    constructor(){
+        this.app = _express_();
+        this.constructAppSettings();
+    }
+
+    private constructAppSettings(): void {
+        this.app.use(_bodyparser_.json());
+        this.app.use(_bodyparser_.urlencoded({ extended: false }));
+    }
+}
+
+const app = new ExpressApp().app;
+
+app.listen(
+    process.env.PORT || PORT, () => 
+    {
+    console.log("Server is valid using port#", PORT);
+    }
+)
